2a03f0526b4a7868bf1a774da786533386ac2658
yuta <eypc.ofun@gmail.com>
2022-04-19 23:53:23 +0900

    add examples

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import TerminalContent from '../../dist'
+
+const App = () => <TerminalContent />
+// eslint-disable-next-line no-undef
+render(<App />, document.getElementById('root'))
